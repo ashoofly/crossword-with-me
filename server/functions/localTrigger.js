e6afe0557ceb566a8b29dbfa46e91445212a75cd
Angela Hsu <ayh123@gmail.com>
2022-07-26 19:26:48 -0600

    added cloud function to fetch daily puzzle

---
+++ server/functions/localTrigger.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+const {PubSub} = require("@google-cloud/pubsub");
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+const pubsub = new PubSub({
+  apiEndpoint: "localhost:8085",
+  projectId: "crossword-with-friends",
+});
+
+const SCHEDULED_FUNCTION_TOPIC = "firebase-schedule-fetchNewPuzzle";
+console.log(`Trigger scheduled function via PubSub topic: 
+            ${SCHEDULED_FUNCTION_TOPIC}`);
+pubsub.topic(SCHEDULED_FUNCTION_TOPIC).publishJSON({
+  foo: "bar",
+}, {attr1: "value1"});
